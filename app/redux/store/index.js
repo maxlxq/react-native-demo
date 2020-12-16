bb0ba83f40c87fd6de6a7b567aa161bc580ce536
向前Hell <542168513@qq.com>
2020-12-16 20:32:54 +0800

    [feature] 2020-12-16: add redux-logger

--- app/redux/store/index.js
+++ app/redux/store/index.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import rootReducer from '@/REDUCER'
 
-let store = createStore(rootReducer)
+const loggerMiddleware = createLogger()
+
+let store = createStore(
+  rootReducer,
+  applyMiddleware(
+    loggerMiddleware,
+  )
+)
 
 export default store
